9a883c9c94898d599baa8c8a88996ef29cbb5424
Adam Huffman <coronablue3@gmail.com>
2025-03-21 17:58:00 -0500

    Implement system for creating/opening task list JSON, and reading it into memory.

--- todo.py
+++ todo.py
@@ -1,4 +1,17 @@
 import sys # For accepting command line arguments
+import json
+
+## Create/open task list JSON file, and read the contents
+tasks_file = "tasks.json"
+with open(tasks_file, "a+") as f:
+    task_string = f.read() # Read the entire file into a variable as a string
+    # Verify that the json file is not empty
+    if task_string != "":
+        tasks = json.loads(task_string) # Convert JSON string into a dict
+    else:
+        tasks = {} # If no task list exists, create an empty dict
+
+print(tasks)
 
 ## Accept user command line arguments
 args = sys.argv[1:] # sys.argv[0] is the script name. Arguments start at index 1

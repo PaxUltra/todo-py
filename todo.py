d1ec34673fd51f6def0a46d8e6beef05545870a3
Adam Huffman <coronablue3@gmail.com>
2025-03-21 18:50:59 -0500

    Add ability to create a task in memory.

--- todo.py
+++ todo.py
@@ -1,5 +1,31 @@
 import sys # For accepting command line arguments
 import json
+from datetime import datetime
+
+## Commands
+def add(args, task_data):
+    name = args[1] # If the command is add, the name will be the second argument
+    description = args[2] # Third argument should be description 
+    id = task_data["next_id"]
+    now = datetime.now() # Current date/time
+    timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S") # YYYY-MM-DD HH:MM:SS
+    new_task = {
+        "id": id,
+        "name": name,
+        "description": description,
+        "status": "todo",
+        "created_at": timestamp_str,
+        "updated_at": timestamp_str
+    }
+
+    # Update next ID, and the task list in memory
+    task_data["next_id"] = task_data["next_id"] + 1
+    task_data["tasks"].append(new_task)
+    print(task_data["tasks"])
+
+commands = {
+    "add": add
+}
 
 ## Create/open task list JSON file, and read the contents
 tasks_file = "tasks.json"
@@ -7,15 +33,22 @@ with open(tasks_file, "a+") as f:
     task_string = f.read() # Read the entire file into a variable as a string
     # Verify that the json file is not empty
     if task_string != "":
-        tasks = json.loads(task_string) # Convert JSON string into a dict
+        task_data = json.loads(task_string) # Convert JSON string into a dict
     else:
-        tasks = {} # If no task list exists, create an empty dict
-
-print(tasks)
+        task_data = {
+            "next_id": 1, # Reset the next ID
+            "tasks": [] # If no task list exists, create an empty list
+        }
 
 ## Accept user command line arguments
 args = sys.argv[1:] # sys.argv[0] is the script name. Arguments start at index 1
-print("Arguments:", args)
+command = args[0] # The command will always be the first argument
+
+# Make sure the command exists
+if command in commands:
+    commands[command](args, task_data)
+else:
+    print("Error: Command not found.")
 
 ## Add a task
 
